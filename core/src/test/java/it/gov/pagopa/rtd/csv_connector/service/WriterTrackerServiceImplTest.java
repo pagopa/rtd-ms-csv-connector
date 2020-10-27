56b5c7f15eb01dea8857e77e84fcb9ee722c3f22
Alessio Cialini <alessio.cialini.sia@gmail.com>
2020-10-27 12:23:23 +0100

    [rtd-ms-csv-connector/develop] Introduced WriterTrackerServiceImplTest

---
+++ core/src/test/java/it/gov/pagopa/rtd/csv_connector/service/WriterTrackerServiceImplTest.java
@@ -0,0 +1,22 @@
+package it.gov.pagopa.rtd.csv_connector.service;
+
+import eu.sia.meda.BaseTest;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.concurrent.CountDownLatch;
+
+public class WriterTrackerServiceImplTest extends BaseTest {
+
+    @Test
+    public void testWriter() {
+        ArrayList<CountDownLatch> countDownLatchArrayList = new ArrayList<>();
+        WriterTrackerServiceImpl writerTrackerService = new WriterTrackerServiceImpl(countDownLatchArrayList);
+        writerTrackerService.addCountDownLatch(new CountDownLatch(1));
+        Assert.assertEquals(1,writerTrackerService.getCountDownLatches().size());
+        writerTrackerService.clearAll();
+        Assert.assertEquals(0,writerTrackerService.getCountDownLatches().size());
+    }
+
+}
