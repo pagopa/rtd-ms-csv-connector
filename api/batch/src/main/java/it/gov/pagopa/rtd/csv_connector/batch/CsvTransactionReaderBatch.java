a0942e259c0982158cf83175d11df6215fa7b358
Alessio Cialini <alessio.cialini.sia@gmail.com>
2021-03-29 15:01:33 +0200

    [rtd-ms-csv-connector/develop] Updated CsvTransactionReaderBatch

--- api/batch/src/main/java/it/gov/pagopa/rtd/csv_connector/batch/CsvTransactionReaderBatch.java
+++ api/batch/src/main/java/it/gov/pagopa/rtd/csv_connector/batch/CsvTransactionReaderBatch.java
@@ -25,6 +25,7 @@ import org.springframework.batch.core.partition.support.MultiResourcePartitioner
 import org.springframework.batch.core.partition.support.Partitioner;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.repository.support.JobRepositoryFactoryBean;
+import org.springframework.batch.core.step.skip.SkipLimitExceededException;
 import org.springframework.batch.core.step.tasklet.TaskletStep;
 import org.springframework.batch.item.ItemProcessor;
 import org.springframework.batch.item.ItemWriter;
@@ -51,10 +52,12 @@ import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 import org.springframework.transaction.PlatformTransactionManager;
 
 import javax.sql.DataSource;
+import javax.validation.ConstraintViolationException;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.time.OffsetDateTime;
 import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 import java.util.Date;
 import java.util.List;
 import java.util.concurrent.Executor;
@@ -400,7 +403,12 @@ public class CsvTransactionReaderBatch {
                 .skipLimit(skipLimit)
                 .noSkip(PGPDecryptException.class)
                 .noSkip(FileNotFoundException.class)
+                .noSkip(SkipLimitExceededException.class)
                 .skip(Exception.class)
+                .noRetry(DateTimeParseException.class)
+                .noRollback(DateTimeParseException.class)
+                .noRetry(ConstraintViolationException.class)
+                .noRollback(ConstraintViolationException.class)
                 .listener(transactionItemReaderListener(executionDate))
                 .listener(transactionsItemWriteListener(executionDate))
                 .listener(transactionsItemProcessListener(executionDate))
